6e82b06da0e014c987956e228cb5f1b6e600e312
Zachary Huff <zach.huff.386@gmail.com>
2016-11-08 22:20:36 -0500

    Set current packages

--- queue/queue.go
+++ queue/queue.go
@@ -2,20 +2,36 @@ package queue
 
 import (
 	"github.com/autoabs/autoabs/pkg"
+	"github.com/dropbox/godropbox/container/set"
 )
 
 type Queue struct {
-	curPackages map[string]*pkg.Package
-	newPackages map[string]*pkg.Package
+	curPackages     map[string]*pkg.Package
+	curPackagesKeys set.Set
+	newPackages     map[string]*pkg.Package
+	newPackagesKeys set.Set
+	addPackages     []*pkg.Package
+	remPackages     []*pkg.Package
 }
 
-func Build() (err error) {
-	newPkgs, err := getNewPackages()
+func (q *Queue) Build() (err error) {
+	q.curPackages = map[string]*pkg.Package{}
+	q.curPackagesKeys = set.NewSet()
+	q.newPackages = map[string]*pkg.Package{}
+	q.newPackagesKeys = set.NewSet()
+	q.addPackages = []*pkg.Package{}
+	q.remPackages = []*pkg.Package{}
+
+	curPkgs, err := getCurPackages()
 	if err != nil {
 		return
 	}
 
-	_ = newPkgs
+	for _, pk := range curPkgs {
+		key := pk.Key()
+		q.curPackages[key] = pk
+		q.curPackagesKeys.Add(key)
+	}
 
 	return
 }
