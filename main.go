07898dbcbe9e8b7aef5d410d6bedf90529903a2e
Zachary Huff <zach.huff.386@gmail.com>
2016-11-08 23:37:05 -0500

    Add main

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/autoabs/autoabs/config"
+	"github.com/autoabs/autoabs/queue"
+)
+
+func main() {
+	err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	que := &queue.Queue{}
+
+	err = que.Queue()
+	if err != nil {
+		panic(err)
+	}
+}
