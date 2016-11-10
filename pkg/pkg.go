d1170e757c277ed3ac7c8e9d5f81b40ff81d5abd
Zachary Huff <zach.huff.386@gmail.com>
2016-11-10 02:01:29 -0500

    Add remove to pkg

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -2,6 +2,12 @@ package pkg
 
 import (
 	"fmt"
+	"github.com/autoabs/autoabs/config"
+	"github.com/autoabs/autoabs/errortypes"
+	"github.com/dropbox/godropbox/errors"
+	"os"
+	"os/exec"
+	"path"
 )
 
 type Package struct {
@@ -21,4 +27,32 @@ func (p *Package) Key() string {
 func (p *Package) Print() {
 	fmt.Printf("%s %s-%s: %s %s %s\n",
 		p.Name, p.Version, p.Release, p.Repo, p.Arch, p.Path)
+}
+
+func (p *Package) RepoPath() string {
+	return path.Join(config.Config.RootPath, "repo", p.Repo, "os", p.Arch,
+		fmt.Sprintf("%s.db.tar.gz", p.Repo))
+}
+
+func (p *Package) Remove() (err error) {
+	if p.Path == "" {
+		return
+	}
+
+	err = exec.Command(
+		"repo-remove",
+		p.RepoPath(),
+		p.Path,
+	).Run()
+	if err != nil {
+		err = errortypes.ExecError{
+			errors.Wrapf(err, "package: Failed to remove package"),
+		}
+		return
+	}
+
+	os.Remove(p.Path)
+	os.Remove(p.Path + ".sig")
+
+	return
 }
