2e924751cc547eedb6cde51195ebabf9043c9f38
Zachary Huff <zach.huff.386@gmail.com>
2016-11-10 17:51:37 -0500

    Add repo add to pkg

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/autoabs/autoabs/config"
 	"github.com/autoabs/autoabs/errortypes"
+	"github.com/autoabs/autoabs/utils"
 	"github.com/dropbox/godropbox/errors"
 	"os"
 	"os/exec"
@@ -31,8 +32,33 @@ func (p *Package) Print() {
 }
 
 func (p *Package) RepoPath() string {
+	return path.Join(config.Config.RootPath, "repo", p.Repo, "os", p.Arch)
+}
+
+func (p *Package) DatabasePath() string {
 	return path.Join(config.Config.RootPath, "repo", p.Repo, "os", p.Arch,
 		fmt.Sprintf("%s.db.tar.gz", p.Repo))
+}
+
+func (p *Package) Add(pkgPath string) (err error) {
+	err = utils.Copy(pkgPath, p.RepoPath())
+	if err != nil {
+		return
+	}
+
+	err = exec.Command(
+		"/usr/bin/repo-add",
+		p.DatabasePath(),
+		pkgPath,
+	).Run()
+	if err != nil {
+		err = errortypes.ExecError{
+			errors.Wrapf(err, "package: Failed to add package"),
+		}
+		return
+	}
+
+	return
 }
 
 func (p *Package) Remove() {
