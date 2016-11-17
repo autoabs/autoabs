db40bacf255d0fe806465230ed1915b3013b3f18
Zachary Huff <zach.huff.386@gmail.com>
2016-11-17 04:03:18 -0500

    Fix formatting in build

--- build/build.go
+++ build/build.go
@@ -7,10 +7,10 @@ import (
 
 type Build struct {
 	Id          bson.ObjectId   `bson:"_id"`
+	Name        string          `bson:"name"`
 	Start       time.Time       `bson:"start"`
 	Stop        time.Time       `bson:"stop"`
 	State       string          `bson:"state"`
-	Name        string          `bson:"name"`
 	Version     string          `bson:"version"`
 	Release     string          `bson:"release"`
 	Repo        string          `bson:"core"`
