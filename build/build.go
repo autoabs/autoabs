a12c5632a159ac88b181d84ba48f2699fe09127c
Zachary Huff <zach.huff.386@gmail.com>
2016-11-17 03:35:11 -0500

    Add pkg builds ids to build

--- build/build.go
+++ build/build.go
@@ -6,14 +6,15 @@ import (
 )
 
 type Build struct {
-	Id      bson.ObjectId `bson:"_id"`
-	Start   time.Time     `bson:"start"`
-	Stop    time.Time     `bson:"stop"`
-	State   string        `bson:"state"`
-	Name    string        `bson:"name"`
-	Version string        `bson:"version"`
-	Release string        `bson:"release"`
-	Repo    string        `bson:"core"`
-	Arch    string        `bson:"arch"`
-	PkgIds  []string      `bson:"pkg_ids"`
+	Id          bson.ObjectId `bson:"_id"`
+	Start       time.Time     `bson:"start"`
+	Stop        time.Time     `bson:"stop"`
+	State       string        `bson:"state"`
+	Name        string        `bson:"name"`
+	Version     string        `bson:"version"`
+	Release     string        `bson:"release"`
+	Repo        string        `bson:"core"`
+	Arch        string        `bson:"arch"`
+	PkgIds      []string      `bson:"pkg_ids"`
+	PkgBuildIds []string      `bson:"pkg_ids"`
 }
