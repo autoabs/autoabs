9567b897e4400e1700ba790b3c476439f919f2e9
Zachary Huff <zach.huff.386@gmail.com>
2016-11-17 03:33:08 -0500

    Add build

---
+++ build/build.go
@@ -0,0 +1,19 @@
+package build
+
+import (
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+type Build struct {
+	Id      bson.ObjectId `bson:"_id"`
+	Start   time.Time     `bson:"start"`
+	Stop    time.Time     `bson:"stop"`
+	State   string        `bson:"state"`
+	Name    string        `bson:"name"`
+	Version string        `bson:"version"`
+	Release string        `bson:"release"`
+	Repo    string        `bson:"core"`
+	Arch    string        `bson:"arch"`
+	PkgIds  []string      `bson:"pkg_ids"`
+}
