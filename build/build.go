2189ac6e0b1f8fadfee65e7e3ce05724319cbec2
Zachary Huff <zach.huff.386@gmail.com>
2016-11-17 19:32:20 -0500

    Add extract to build

--- build/build.go
+++ build/build.go
@@ -1,7 +1,16 @@
 package build
 
 import (
+	"archive/tar"
+	"github.com/autoabs/autoabs/config"
+	"github.com/autoabs/autoabs/database"
+	"github.com/autoabs/autoabs/errortypes"
+	"github.com/dropbox/godropbox/errors"
+	"github.com/pacur/pacur/utils"
 	"gopkg.in/mgo.v2/bson"
+	"io"
+	"os"
+	"path"
 	"time"
 )
 
@@ -17,4 +26,72 @@ type Build struct {
 	Arch       string          `bson:"arch"`
 	PkgIds     []bson.ObjectId `bson:"pkg_ids"`
 	PkgBuildId bson.ObjectId   `bson:"pkg_build_id"`
+}
+
+func (b *Build) tmpPath() string {
+	return path.Join(config.Config.RootPath, "tmp",
+		b.Name+"-"+b.Repo+"-"+b.Arch+"-"+b.Version+"-"+b.Release)
+}
+
+func (b *Build) extract(db *database.Database) (err error) {
+	tmpPath := b.tmpPath()
+
+	gfs := db.PkgBuildGrid()
+
+	gf, err := gfs.OpenId(b.PkgBuildId)
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	arc := tar.NewReader(gf)
+
+	for {
+		hdr, e := arc.Next()
+		if e == io.EOF {
+			break
+		}
+		if e != nil {
+			e = &errortypes.ReadError{
+				errors.Wrap(err, "build: Failed to read tar header"),
+			}
+			return
+		}
+
+		pth := path.Join(tmpPath, hdr.Name)
+		dirPth := path.Dir(pth)
+
+		err = utils.ExistsMakeDir(dirPth)
+		if err != nil {
+			return
+		}
+
+		file, e := os.OpenFile(
+			pth,
+			os.O_RDWR|os.O_CREATE|os.O_TRUNC,
+			os.FileMode(hdr.Mode),
+		)
+		if e != nil {
+			e = &errortypes.WriteError{
+				errors.Wrap(err, "build: Failed to write tar file"),
+			}
+			return
+		}
+
+		_, err = io.Copy(file, arc)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func (b *Build) Build(db *database.Database) (err error) {
+	err = b.extract(db)
+	if err != nil {
+		return
+	}
+
+	return
 }
