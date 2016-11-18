7be62ec743851f81c494002bbc90f4c3742bfc01
Zachary Huff <zach.huff.386@gmail.com>
2016-11-18 02:46:57 -0500

    Remove build from pkg

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -2,25 +2,19 @@ package pkg
 
 import (
 	"archive/tar"
-	"bufio"
 	"fmt"
-	"github.com/Sirupsen/logrus"
 	"github.com/autoabs/autoabs/build"
 	"github.com/autoabs/autoabs/config"
-	"github.com/autoabs/autoabs/constants"
 	"github.com/autoabs/autoabs/database"
 	"github.com/autoabs/autoabs/errortypes"
 	"github.com/autoabs/autoabs/utils"
 	"github.com/dropbox/godropbox/errors"
 	"gopkg.in/mgo.v2/bson"
 	"io"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
 	"path/filepath"
-	"strings"
-	"sync"
 )
 
 type Package struct {
@@ -53,11 +47,6 @@ func (p *Package) DatabasePath() string {
 
 func (p *Package) BuildPath() string {
 	return path.Join(config.Config.RootPath, "builds",
-		p.Name+"-"+p.Repo+"-"+p.Arch+"-"+p.Version+"-"+p.Release)
-}
-
-func (p *Package) TmpPath() string {
-	return path.Join(config.Config.RootPath, "tmp",
 		p.Name+"-"+p.Repo+"-"+p.Arch+"-"+p.Version+"-"+p.Release)
 }
 
@@ -179,170 +168,6 @@ func (p *Package) QueueBuild() (err error) {
 		}
 		return
 	}
-
-	return
-}
-
-func (p *Package) Build() (err error) {
-	buildPath := p.BuildPath()
-	tmpPath := p.TmpPath()
-
-	defer utils.ExistsRemove(tmpPath)
-
-	logrus.WithFields(logrus.Fields{
-		"package": p.Name,
-	}).Info("profile: Building package")
-
-	err = utils.ExistsRemove(tmpPath)
-	if err != nil {
-		return
-	}
-
-	err = utils.CopyAll(buildPath, tmpPath)
-	if err != nil {
-		return
-	}
-
-	cmd := exec.Command(
-		"/usr/bin/docker",
-		"run",
-		"--rm",
-		"-v", tmpPath+":/pkg",
-		constants.BuildImage,
-	)
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		err = &errortypes.ExecError{
-			errors.Wrap(err, "pkg: Failed to get stdout"),
-		}
-		return
-	}
-
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		err = &errortypes.ExecError{
-			errors.Wrap(err, "pkg: Failed to get stderr"),
-		}
-		return
-	}
-
-	logPath := p.LogPath()
-	utils.ExistsRemove(logPath)
-
-	err = utils.ExistsMkdir(logPath, 0755)
-	if err != nil {
-		return
-	}
-
-	logPath = path.Join(logPath, "build.log")
-	logLock := sync.Mutex{}
-	logFile, err := os.OpenFile(logPath,
-		os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		err = &errortypes.WriteError{
-			errors.Wrapf(err, "pkg: Failed to open file %s", logPath),
-		}
-		return
-	}
-	defer logFile.Close()
-
-	go func() {
-		out := bufio.NewReader(stdout)
-		for {
-			line, _, err := out.ReadLine()
-			if err != nil {
-				if !strings.Contains(
-					err.Error(), "bad file descriptor") && err != io.EOF {
-
-					err = &errortypes.ReadError{
-						errors.Wrap(err, "profile: Failed to read stdout"),
-					}
-					logrus.WithFields(logrus.Fields{
-						"error": err,
-					}).Error("profile: Stdout error")
-				}
-
-				return
-			}
-
-			logLock.Lock()
-			_, err = logFile.WriteString(string(line) + "\n")
-			if err != nil {
-				logrus.WithFields(logrus.Fields{
-					"error":    err,
-					"log_path": logPath,
-				}).Error("profile: Failed to write file")
-			}
-			logLock.Unlock()
-		}
-	}()
-
-	go func() {
-		out := bufio.NewReader(stderr)
-		for {
-			line, _, err := out.ReadLine()
-			if err != nil {
-				if !strings.Contains(
-					err.Error(), "bad file descriptor") && err != io.EOF {
-
-					err = &errortypes.ReadError{
-						errors.Wrap(err, "profile: Failed to read stderr"),
-					}
-					logrus.WithFields(logrus.Fields{
-						"error": err,
-					}).Error("profile: Stderr error")
-				}
-
-				return
-			}
-
-			logLock.Lock()
-			_, err = logFile.WriteString(string(line) + "\n")
-			if err != nil {
-				logrus.WithFields(logrus.Fields{
-					"error":    err,
-					"log_path": logPath,
-				}).Error("profile:  Failed to write file")
-			}
-			logLock.Unlock()
-		}
-	}()
-
-	err = cmd.Start()
-	if err != nil {
-		err = &errortypes.ExecError{
-			errors.Wrap(err, "pkg: Failed to build"),
-		}
-		return
-	}
-
-	err = cmd.Wait()
-	if err != nil {
-		err = &errortypes.ExecError{
-			errors.Wrap(err, "pkg: Build error"),
-		}
-		return
-	}
-
-	files, err := ioutil.ReadDir(tmpPath)
-	if err != nil {
-		err = &errortypes.ReadError{
-			errors.Wrapf(err, "pkg: Failed to read dir %s", tmpPath),
-		}
-		return
-	}
-
-	for _, file := range files {
-		if strings.HasSuffix(file.Name(), constants.PackageExt) {
-			err = p.Add(path.Join(tmpPath, file.Name()))
-			if err != nil {
-				return
-			}
-		}
-	}
-
-	utils.ExistsRemove(buildPath)
 
 	return
 }
