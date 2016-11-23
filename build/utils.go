d916a26252855169b20d856fb09cf099da0d7169
Zachary Huff <zach.huff.386@gmail.com>
2016-11-22 19:21:38 -0500

    Add clear failed and pending to build utils

--- build/utils.go
+++ build/utils.go
@@ -45,3 +45,43 @@ func ClearAllBuilds() (err error) {
 
 	return
 }
+
+func ClearPendingBuilds() (err error) {
+	db := database.GetDatabase()
+	defer db.Close()
+	coll := db.Builds()
+
+	cursor := coll.Find(&bson.M{
+		"state": "pending",
+	}).Iter()
+
+	bild := &Build{}
+	for cursor.Next(bild) {
+		err = bild.Remove(db)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func ClearFailedBuilds() (err error) {
+	db := database.GetDatabase()
+	defer db.Close()
+	coll := db.Builds()
+
+	cursor := coll.Find(&bson.M{
+		"state": "failed",
+	}).Iter()
+
+	bild := &Build{}
+	for cursor.Next(bild) {
+		err = bild.Remove(db)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
