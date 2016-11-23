7b46d9a5f8bad31595c2ae82bdf5dbd6ad17b217
Zachary Huff <zach.huff.386@gmail.com>
2016-11-22 19:35:16 -0500

    Handle unknown cmd

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/autoabs/autoabs/cmd"
 	"github.com/autoabs/autoabs/requires"
+	"os"
 )
 
 func main() {
@@ -14,25 +16,35 @@ func main() {
 	switch flag.Arg(0) {
 	case "app":
 		cmd.App()
+		return
 	case "set":
 		cmd.Settings()
+		return
 	case "builds":
 		switch flag.Arg(1) {
 		case "sync":
 			cmd.Sync()
+			return
 		case "build":
 			cmd.Build()
+			return
 		case "clear":
 			switch flag.Arg(2) {
 			case "all":
 				cmd.ClearAll()
+				return
 			case "pending":
 				cmd.ClearPending()
+				return
 			case "failed":
 				cmd.ClearFailed()
+				return
 			}
 		}
 	case "genkey":
 		cmd.GenKey()
+		return
 	}
+
+	fmt.Fprintln(os.Stderr, "Unknown command")
 }
