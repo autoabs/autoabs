a7a1e0cc0f0969368d621f2180e8ee90df2b4d82
Zachary Huff <zach.huff.386@gmail.com>
2016-11-25 03:58:17 -0500

    Fix formatting in constants

--- constants/constants.go
+++ constants/constants.go
@@ -5,10 +5,12 @@ import (
 )
 
 const (
-	StaticLive  = true
 	StaticRoot  = "www"
+	StaticLive  = true
 	StaticCache = false
 	PackageExt  = "pkg.tar.xz"
 	BuildImage  = "builder"
 	RetryDelay  = 3 * time.Second
 )
+
+// DB=mongodb://pritunl:s9T82kD8JVLm@db0.pritunl.net:27017/autoabs?ssl=true
