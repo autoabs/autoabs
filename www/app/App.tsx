8aeb01c48292090dca3b9ad9efc689da8cdc3bc5
Zachary Huff <zach.huff.386@gmail.com>
2016-11-26 03:43:00 -0500

    Inject tap plugin

--- www/app/App.tsx
+++ www/app/App.tsx
@@ -1,7 +1,10 @@
 /// <reference path="References.d.ts"/>
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
 import Main from './components/Main';
+
+injectTapEventPlugin();
 
 ReactDOM.render(
 	<Main/>,
