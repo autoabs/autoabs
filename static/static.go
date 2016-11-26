c7451b5119340f1bf460aec7fe21c581f0b0bcbe
Zachary Huff <zach.huff.386@gmail.com>
2016-11-26 02:56:08 -0500

    Add mimetypes

--- static/static.go
+++ static/static.go
@@ -16,19 +16,21 @@ import (
 
 var (
 	mimeTypes = map[string]string{
-		".js":   "application/javascript",
-		".json": "application/json",
-		".css":  "text/css",
-		".html": "text/html",
-		".jpg":  "image/jpeg",
-		".png":  "image/png",
-		".svg":  "image/svg+xml",
-		".ico":  "image/vnd.microsoft.icon",
-		".otf":  "application/font-sfnt",
-		".ttf":  "application/font-sfnt",
-		".woff": "application/font-woff",
-		".eot":  "application/vnd.ms-fontobject",
-		".map":  "application/json",
+		".js":    "application/javascript",
+		".json":  "application/json",
+		".css":   "text/css",
+		".html":  "text/html",
+		".jpg":   "image/jpeg",
+		".png":   "image/png",
+		".svg":   "image/svg+xml",
+		".ico":   "image/vnd.microsoft.icon",
+		".otf":   "application/font-sfnt",
+		".ttf":   "application/font-sfnt",
+		".woff":  "application/font-woff",
+		".woff2": "font/woff2",
+		".ijmap": "text/plain",
+		".eot":   "application/vnd.ms-fontobject",
+		".map":   "application/json",
 	}
 )
 
