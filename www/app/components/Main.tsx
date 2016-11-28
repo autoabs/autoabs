a07a13f497014649462204e33c6fdc33ae74c6d0
Zachary Huff <zach.huff.386@gmail.com>
2016-11-28 01:18:29 -0500

    Add alert to main

--- www/app/components/Main.tsx
+++ www/app/components/Main.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Styles from '../Styles';
+import Alerts from './Alerts';
 import Builds from './Builds';
 
 document.body.style.backgroundColor = Styles.colors.background;
@@ -9,7 +10,10 @@ document.body.style.backgroundColor = Styles.colors.background;
 export default class Main extends React.Component<void, void> {
 	render(): JSX.Element {
 		return <MuiThemeProvider muiTheme={Styles.theme}>
-			<Builds/>
+			<div>
+				<Builds/>
+				<Alerts/>
+			</div>
 		</MuiThemeProvider>;
 	}
 }
