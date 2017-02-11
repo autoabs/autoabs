4a2668e4ed5360bd8ba571f7c4520678e92b54ce
Zachary Huff <zach.huff.386@gmail.com>
2017-02-11 01:28:32 -0500

    Add focus manager to app

--- www/app/App.tsx
+++ www/app/App.tsx
@@ -1,7 +1,10 @@
 /// <reference path="References.d.ts"/>
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as Blueprint from '@blueprintjs/core';
 import Main from './components/Main';
+
+Blueprint.FocusStyleManager.onlyShowFocusOnTabs();
 
 ReactDOM.render(
 	<Main/>,
