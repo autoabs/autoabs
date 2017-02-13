f19804f8580d2dd4b336852d247cb87ea7bbd2a6
Zachary Huff <zach.huff.386@gmail.com>
2017-02-13 05:14:57 -0500

    Add loading component

---
+++ www/app/components/Loading.tsx
@@ -0,0 +1,52 @@
+/// <reference path="../References.d.ts"/>
+import * as React from 'react';
+import * as Blueprint from '@blueprintjs/core';
+import LoadingStore from '../stores/LoadingStore';
+
+interface Props {
+	size?: string;
+	intent?: Blueprint.Intent,
+}
+
+interface State {
+	loading: boolean;
+}
+
+export default class Loading extends React.Component<Props, State> {
+	constructor(props: Props, context: any) {
+		super(props, context);
+		this.state = {
+			loading: LoadingStore.loading,
+		};
+	}
+
+	componentDidMount(): void {
+		LoadingStore.addChangeListener(this.onChange);
+	}
+
+	componentWillUnmount(): void {
+		LoadingStore.removeChangeListener(this.onChange);
+	}
+
+	onChange = (): void => {
+		this.setState({
+			loading: LoadingStore.loading,
+		});
+	}
+
+	render(): JSX.Element {
+		if (!this.state.loading) {
+			return null;
+		}
+
+		let className = '';
+		if (this.props.size) {
+			className = 'pt-' + this.props.size;
+		}
+
+		return <Blueprint.Spinner
+			className={className}
+			intent={this.props.intent}
+		/>;
+	}
+}
