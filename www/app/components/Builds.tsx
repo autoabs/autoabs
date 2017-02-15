eecba1104d518d10283b8831656ae0125d11f4e4
Zachary Huff <zach.huff.386@gmail.com>
2017-02-14 23:42:01 -0500

    Remove unused import in builds

--- www/app/components/Builds.tsx
+++ www/app/components/Builds.tsx
@@ -1,6 +1,5 @@
 /// <reference path="../References.d.ts"/>
 import * as React from 'react';
-import * as Blueprint from '@blueprintjs/core';
 import * as BuildTypes from '../types/BuildTypes';
 import BuildStore from '../stores/BuildStore';
 import InfiniteFlex from './InfiniteFlex';
