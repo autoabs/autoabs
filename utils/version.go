507469d511e66095560c61ff16df006f1f81d981
Zachary Huff <zach.huff.386@gmail.com>
2017-03-03 04:22:24 -0500

    Add version utils

---
+++ utils/version.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func VersionNewer(x, y string) bool {
+	cmd := exec.Command("vercmp", x, y)
+
+	output, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	n, err := strconv.Atoi(strings.TrimSpace(string(output)))
+	if err != nil {
+		panic(err)
+	}
+
+	if n > 0 {
+		return true
+	}
+
+	return false
+}
