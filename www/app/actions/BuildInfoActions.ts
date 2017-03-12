c20bbecdb6eb8d018457f00311499ca644154048
Zachary Huff <zach.huff.386@gmail.com>
2017-03-11 20:32:03 -0500

    Add sync id to build info actions

--- www/app/actions/BuildInfoActions.ts
+++ www/app/actions/BuildInfoActions.ts
@@ -5,11 +5,17 @@ import * as Alert from '../Alert';
 import Loader from '../Loader';
 import * as BuildInfoTypes from '../types/BuildInfoTypes';
 import BuildInfoStore from '../stores/BuildInfoStore';
+import * as MiscUtils from '../utils/MiscUtils';
+
+let syncId: string;
 
 export function sync(): Promise<void> {
 	if (!BuildInfoStore.id) {
 		return Promise.resolve();
 	}
+
+	let curSyncId = MiscUtils.uuid();
+	syncId = curSyncId;
 
 	let loader = new Loader().loading();
 
@@ -19,6 +25,11 @@ export function sync(): Promise<void> {
 			.set('Accept', 'application/json')
 			.end((err: any, res: SuperAgent.Response): void => {
 				loader.done();
+
+				if (curSyncId !== syncId) {
+					resolve();
+					return;
+				}
 
 				if (err) {
 					Alert.error('Failed to get build log');
