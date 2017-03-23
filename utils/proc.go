c2e1c5b6418f4488e9df4442f474b5b9c4409cdf
Zachary Huff <zach.huff.386@gmail.com>
2017-03-22 21:02:50 -0400

    Fix formatting in proc

--- utils/proc.go
+++ utils/proc.go
@@ -37,7 +37,8 @@ func ExecInput(dir, input, name string, arg ...string) (err error) {
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		err = &errortypes.ExecError{
-			errors.Wrapf(err, "utils: Failed to get stdin in exec '%s'", name),
+			errors.Wrapf(err,
+				"utils: Failed to get stdin in exec '%s'", name),
 		}
 		return
 	}
