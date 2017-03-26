a4dd9a84fcc33baf8b260049d274eebacd973e44
Zachary Huff <zach.huff.386@gmail.com>
2017-03-26 05:09:01 -0400

    Add json tags to node

--- node/node.go
+++ node/node.go
@@ -9,12 +9,12 @@ import (
 )
 
 type Node struct {
-	Name      string    `bson:"_id"`
-	Timestamp time.Time `bson:"timestamp"`
-	Memory    float64   `bson:"memory"`
-	Load1     float64   `bson:"load1"`
-	Load5     float64   `bson:"load5"`
-	Load15    float64   `bson:"load15"`
+	Name      string    `bson:"_id" json:"name"`
+	Timestamp time.Time `bson:"timestamp" json:"timestamp"`
+	Memory    float64   `bson:"memory" json:"memory"`
+	Load1     float64   `bson:"load1" json:"load1"`
+	Load5     float64   `bson:"load5" json:"load5"`
+	Load15    float64   `bson:"load15" json:"load15"`
 }
 
 func (n *Node) keepalive() {
