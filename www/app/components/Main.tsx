060aa625b92fcbbcd81d7524691a235afd73571f
Zachary Huff <zach.huff.386@gmail.com>
2017-03-26 18:50:55 -0400

    Remove container fix

--- www/app/components/Main.tsx
+++ www/app/components/Main.tsx
@@ -4,26 +4,8 @@ import Builds from './Builds';
 
 document.body.className = 'root pt-dark';
 
-interface Props {
-	children?: JSX.Element;
-}
-
-class ContainerFix extends React.Component<Props, void> {
-	static childContextTypes = {};
-
-	getChildContext() {
-		return {};
-	}
-
-	render(): JSX.Element {
-		return this.props.children;
-	}
-}
-
 export default class Main extends React.Component<void, void> {
 	render(): JSX.Element {
-		return <ContainerFix>
-			<Builds/>
-		</ContainerFix>;
+		return <Builds/>;
 	}
 }
