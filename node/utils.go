b2ee89e94b576fcafdddd445d5464180d13f1d6d
Zachary Huff <zach.huff.386@gmail.com>
2017-03-26 05:06:38 -0400

    Add node utils

---
+++ node/utils.go
@@ -0,0 +1,27 @@
+package node
+
+import (
+	"github.com/blckur/blckur/database"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func GetAll(db *database.Database) (nodes []*Node, err error) {
+	nodes = []*Node{}
+	coll := db.Nodes()
+
+	cursor := coll.Find(&bson.M{}).Iter()
+
+	nde := &Node{}
+	for cursor.Next(nde) {
+		nodes = append(nodes, nde)
+		nde = &Node{}
+	}
+
+	err = cursor.Close()
+	if err != nil {
+		err = database.ParseError(err)
+		return
+	}
+
+	return
+}
