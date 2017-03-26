ba6c16eec7cec69b24ee85212dbc89289fade6c3
Zachary Huff <zach.huff.386@gmail.com>
2017-03-26 18:54:28 -0400

    Add nav to main

--- www/app/components/Main.tsx
+++ www/app/components/Main.tsx
@@ -1,11 +1,60 @@
 /// <reference path="../References.d.ts"/>
 import * as React from 'react';
+import * as BuildActions from '../actions/BuildActions';
+import Loading from './Loading';
 import Builds from './Builds';
+import Nodes from './Nodes';
 
 document.body.className = 'root pt-dark';
 
-export default class Main extends React.Component<void, void> {
+interface State {
+	page: string;
+}
+
+export default class Main extends React.Component<void, State> {
+	constructor(props: any, context: any) {
+		super(props, context);
+		this.state = {
+			page: 'builds',
+		};
+	}
+
+	sync = (): void => {
+		switch (this.state.page) {
+			case 'builds':
+				BuildActions.sync();
+				break;
+		}
+	}
+
 	render(): JSX.Element {
-		return <Builds/>;
+		let page: JSX.Element;
+
+		switch (this.state.page) {
+			case 'builds':
+				page = <Builds/>;
+				break;
+			case 'nodes':
+				page = <Nodes/>;
+				break;
+		}
+
+		return <div>
+			<nav className="pt-navbar">
+				<div className="pt-navbar-group pt-align-left">
+					<div className="pt-navbar-heading">AutoABS</div>
+					<Loading size="small"/>
+					<div className="pt-navbar-heading">Builds</div>
+					<div className="pt-navbar-heading">Nodes</div>
+				</div>
+				<div className="pt-navbar-group pt-align-right">
+					<button
+						className="pt-button pt-minimal pt-icon-refresh"
+						onClick={this.sync}
+					/>
+				</div>
+			</nav>
+			{page}
+		</div>;
 	}
 }
