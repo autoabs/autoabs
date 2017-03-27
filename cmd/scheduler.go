3169ded6214379a3267e6681c19fb6d5567e685b
Zachary Huff <zach.huff.386@gmail.com>
2017-03-27 02:19:22 -0400

    Init node in scheduler

--- cmd/scheduler.go
+++ cmd/scheduler.go
@@ -1,16 +1,30 @@
 package cmd
 
 import (
+	"github.com/autoabs/autoabs/node"
 	"github.com/autoabs/autoabs/scheduler"
+	"github.com/autoabs/autoabs/utils"
 )
 
 func StorageScheduler() {
+	nde := node.Node{
+		Id:   utils.RandName(),
+		Type: "storage",
+	}
+	nde.Keepalive()
+
 	sch := scheduler.Storage{}
 
 	sch.Start()
 }
 
 func BuildScheduler() {
+	nde := node.Node{
+		Id:   utils.RandName(),
+		Type: "builder",
+	}
+	nde.Keepalive()
+
 	sch := scheduler.Build{}
 
 	sch.Start()
