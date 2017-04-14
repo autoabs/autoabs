5668f0357335d7c75942624cc372ec7364cde256
Zachary Huff <zach.huff.386@gmail.com>
2017-04-14 19:42:03 -0400

    Add get to node utils

--- node/utils.go
+++ node/utils.go
@@ -5,6 +5,23 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+func Get(db *database.Database, nodeId string) (nde *Node, err error) {
+	nde = &Node{}
+	coll := db.Nodes()
+
+	err = coll.FindOneId(nodeId, nde)
+	if err != nil {
+		return
+	}
+
+	err = nde.LoadSettings(db)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func GetAll(db *database.Database) (nodes []*Node, err error) {
 	nodes = []*Node{}
 	coll := db.Nodes()
