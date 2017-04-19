43f96f2d54ccff4e3c48f06b565c88da3c9da208
Zachary Huff <zach.huff.386@gmail.com>
2017-04-19 03:12:28 -0400

    Add stats to node

--- node/builder.go
+++ node/builder.go
@@ -1,5 +1,9 @@
 package node
 
+type BuilderStats struct {
+	Active int `bson:"active" json:"active"`
+}
+
 type BuilderSettings struct {
 	NodeId      string `bson:"_id" json:"-"`
 	Concurrency int    `bson:"concurrency" json:"concurrency"`
--- node/node.go
+++ node/node.go
@@ -18,6 +18,7 @@ type Node struct {
 	Load1     float64     `bson:"load1" json:"load1"`
 	Load5     float64     `bson:"load5" json:"load5"`
 	Load15    float64     `bson:"load15" json:"load15"`
+	Stats     interface{} `bson:"stats" json:"stats"`
 	Settings  interface{} `bson:"settings" json:"settings"`
 }
 
